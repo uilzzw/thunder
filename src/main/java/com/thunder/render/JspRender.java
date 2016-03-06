5c451b93a65672658d1b5e3d90ecfab153c228ec
icepoint0 <351711778@qq.com>
2016-03-06 21:57:45 +0800

    change env

--- src/main/java/com/thunder/render/JspRender.java
+++ src/main/java/com/thunder/render/JspRender.java
@@ -14,7 +14,7 @@ import java.io.Writer;
  * Created by icepoint1999 on 3/1/16.
  */
 public class JspRender  implements  Render{
-    
+
     public  void render(String view, Writer writer) {
 
     String viewPath = PathUtil.getViewPath(view);
